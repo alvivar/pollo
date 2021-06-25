8ae0e3750171b05c62caead1a87854f3c563dc98
Andrés Villalobos <andresalvivar@gmail.com>
2021-06-25 14:49:04 -0600

    Comments +1

--- src/parse.rs
+++ src/parse.rs
@@ -4,32 +4,43 @@ pub struct Msg {
     pub value: String,
 }
 
+/// Returns a Msg with the first character found as op, the next word as key,
+/// and the rest as value.
+///
+/// This text: +hello world is a pretty old meme
+/// Returns: Msg { "+", "hello", "world is a pretty old meme" }
 pub fn parse(text: &str) -> Msg {
     let mut op = String::new();
     let mut key = String::new();
     let mut value = String::new();
 
-    let mut deep = 0;
+    let mut next = 0;
     for c in text.chars() {
         match c {
             ' ' => {
                 if !value.is_empty() {
                     value.push(' ');
                 } else if !key.is_empty() {
-                    deep = 2;
+                    next = 2;
                 } else if !op.is_empty() {
-                    deep = 1;
+                    next = 1;
                 }
             }
 
-            _ => match deep {
+            _ => match next {
                 0 => {
                     op.push(c);
+
+                    // The firts non-space found is the (op)erator. Separated or
+                    // not by a space.
+                    next = 1;
                 }
                 1 => {
                     key.push(c);
                 }
                 _ => {
+                    // @doubt There may be a way to push the rest of the
+                    // iterator instead of moving one by one after this point.
                     value.push(c);
                 }
             },
